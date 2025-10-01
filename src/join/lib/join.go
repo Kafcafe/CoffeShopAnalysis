65f8e97f0ad1d52112463e3ac3acd74c2bdef0e3
Ignacio <j14castro2001@gmail.com>
2025-10-01 00:25:45 -0300

    add join docs

--- src/join/lib/join.go
+++ src/join/lib/join.go
@@ -11,6 +11,19 @@ func NewJoiner() *Join {
 	return &Join{}
 }
 
+// rightTable => In memory table, the one that will turn to a map
+//
+// leftTable => The one that will be iterated
+//
+// rightIndex => The index of the field that will replace the left field
+//
+// rightIdIndex => The index of the field that will be used to match with the left table
+//
+// leftIndex => The index of the field that will be replaced
+//
+// Example: JoinByIndex(stores, sales, 1, 0, 2) => In stores, index 1 is the name of the store, index 0 is the id of the store. In sales, index 2 is the id of the store
+//
+// Result: The sales table will have the name of the store instead of the id of the store
 func (j *Join) JoinByIndex(rightTable []string, leftTable []string, rightIndex, rightIdIndex, leftIndex int) []string {
 	joinedItems := make([]string, 0)
 	if len(rightTable) == 0 || len(leftTable) == 0 {
