159f4baa87d79473c9effbb2f6e06074f9ea7ae2
ramiro gestoso <rgestoso@fi.uba.ar>
2025-10-01 00:56:54 -0300

    feat: add join worker

--- src/join/main.go
+++ src/join/main.go
@@ -1,12 +1,106 @@
 package main
 
 import (
+	logger "common/logger"
+	middleware "common/middleware"
+	"fmt"
 	join "join/lib"
+	"os"
+	"strings"
+
+	"github.com/op/go-logging"
+	"github.com/spf13/viper"
 )
 
-func main() {
-	joiner := join.NewJoiner()
-	if joiner == nil {
-		panic("Joiner is nil")
+const (
+	SUCCESS_EXIT_CODE                 = 0
+	STARTUP_ERROR_EXIT_CODE           = 1
+	ERROR_DURING_PROCESSING_EXIT_CODE = 2
+)
+
+// InitConfig initializes the application configuration using Viper.
+// It reads from config.yaml and environment variables with CLI_ prefix.
+// Returns the configured Viper instance or an error.
+func InitConfig() (*viper.Viper, error) {
+
+	v := viper.New()
+
+	// Configure viper to read env variables with the CLI_ prefix
+	v.AutomaticEnv()
+	//v.SetEnvPrefix("cli")
+	// Use a replacer to replace env variables underscores with points. This let us
+	// use nested configurations in the config file and at the same time define
+	// env variables for the nested configurations
+	v.SetEnvKeyReplacer(strings.NewReplacer(".", "_"))
+
+	// Try to read configuration from config file. If config file
+	// does not exists then ReadInConfig will fail but configuration
+	// can be loaded from the environment variables so we shouldn't
+	// return an error in that case
+	v.SetConfigFile("./config.yaml")
+	if err := v.ReadInConfig(); err != nil {
+		fmt.Printf("Configuration could not be read from config file. Using env variables instead")
 	}
+
+	return v, nil
+}
+
+// PrintConfig logs the current server configuration details.
+// Parameters:
+//
+//	v: the configuration instance
+func PrintConfig(v *viper.Viper, logger *logging.Logger) {
+	logger.Infof("GroupBy startup with: type %s | id: %s | groupByCount : %d",
+		v.GetString("group.type"), v.GetString("group.id"), v.GetInt("group.count"),
+	)
+
+	logger.Infof("Detected RabbitMQ configuration: host: %s | port: %d | username: %s | password: %s",
+		v.GetString("rabbitmq.host"),
+		v.GetInt("rabbitmq.port"),
+		v.GetString("rabbitmq.user"),
+		v.GetString("rabbitmq.pass"),
+	)
+}
+
+func main() {
+	config, err := InitConfig()
+	if err != nil {
+		fmt.Printf("Error initializing configuration: %v\n", err)
+		return
+	}
+
+	err = logger.InitGlobalLogger(config.GetString("log.level"))
+	if err != nil {
+		fmt.Printf("Error initializing logger: %v\n", err)
+		return
+	}
+
+	logger := logger.GetLoggerWithPrefix("[MAIN]")
+
+	PrintConfig(config, logger)
+
+	rabbitConf := middleware.NewRabbitConfig(
+		config.GetString("rabbitmq.user"),
+		config.GetString("rabbitmq.pass"),
+		config.GetString("rabbitmq.host"),
+		config.GetInt("rabbitmq.port"),
+	)
+
+	joinerId := config.GetString("joiner.id")
+	joinerCount := config.GetInt("joiner.count")
+	joinerType := config.GetString("joiner.type")
+
+	joinItemsWorker, err := join.CreateJoinItemsWorker(joinerType, rabbitConf, joinerId, joinerCount)
+	if err != nil {
+		logger.Errorf("Failed creating new joinItems worker: %s", err)
+		os.Exit(STARTUP_ERROR_EXIT_CODE)
+	}
+
+	err = (*joinItemsWorker).Run()
+	if err != nil {
+		logger.Errorf("Failed creating new joinerBy worker: %s", err)
+		os.Exit(ERROR_DURING_PROCESSING_EXIT_CODE)
+	}
+
+	os.Exit(SUCCESS_EXIT_CODE)
 }
