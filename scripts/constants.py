6d30fd40de6ce5048ba90e90c49a44a3fd1a234c
Ignacio <j14castro2001@gmail.com>
2025-10-15 12:49:32 -0300

    feat: ajust volume

--- scripts/constants.py
+++ scripts/constants.py
@@ -119,7 +119,7 @@ CLIENTS_TEMPLATE = """
         volumes:
           - ./src/client/config.yaml:/config.yaml
           - ./results:/results
-          - ./.data:/data
+          - ./testData:/data
 """
 
 GROUP_TEMPLATE = """
