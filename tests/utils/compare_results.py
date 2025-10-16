0ec72384195b41936d6eeb19dcbf1cbf9bb80bc2
Ignacio <j14castro2001@gmail.com>
2025-10-16 19:01:27 -0300

    update compare results

--- tests/utils/compare_results.py
+++ tests/utils/compare_results.py
@@ -36,65 +36,38 @@ def compare_results_q1(q1_path):
 
     with open(q1_path, "r", encoding="utf-8") as f:
         for line in f:
-            id, _, _, amount, _ = line.strip().split(',')
+            id, amount = line.strip().split(',')
             formatted = f"{id},{float(amount):.1f}"
-            lines_current_results.add(formatted)
+            lines_expected_results.add(formatted)
     
     with open(expected_results, "r", encoding="utf-8") as f:
         next(f)
         for line in f:
-            id, _, _, amount, _ = line.strip().split(',')
+            id, amount = line.strip().split(',')
             formatted = f"{id},{float(amount):.1f}"
             lines_expected_results.add(formatted)
 
     return _compare_sets(name, lines_current_results, lines_expected_results)
 
 
-def compare_results_q2_top_earners(q2_path):
-    name = "results_q2_top_earners"
-    expected_results_path = "./tests/expected_results/results_q2_top_earners.csv"
-
-    current_results = set()
-    expected_results = set()
-    
-    with open(q2_path, 'r') as file:
-        for line in file:
-            yearMonth, item, qty, profit = line.strip().split(',')
-            if not float(profit) > 0:
-                continue
-            current_results.add(f"{yearMonth},{item},{float(profit):.2f}")
-
-    with open(expected_results_path, 'r') as file:
-        next(file)
-        for line in file:
-            yearMonth, item, profit = line.strip().split(',')
-            expected_results.add(f"{yearMonth},{item},{float(profit):.2f}")
-
-    return _compare_sets(name, current_results, expected_results)
-
-
-def compare_results_q2_best_sellers(q2_path):
+def compare_results_q2(client_id):
     name = "results_q2_best_sellers"
-    expected_results_path = "./tests/expected_results/results_q2_best_sellers.csv"
+    current_results_path = f"./results/results_q2_{client_id}.txt"
+    expected_results_path = "./scripts/expected_results/results_q2.csv"
 
     current_results = set()
     expected_results = set()
     
-    with open(q2_path, 'r') as file:
+    with open(current_results_path, 'r') as file:
         for line in file:
-            yearMonth, item, qty, profit = line.strip().split(',')
-            if not int(qty) > 0:
-                continue
-            current_results.add(f"{yearMonth},{item},{qty}")
+            current_results.add(line.strip())
 
     with open(expected_results_path, 'r') as file:
-        next(file)
         for line in file:
-            yearMonth, item, qty = line.strip().split(',')
-            expected_results.add(f"{yearMonth},{item},{qty}")
+            expected_results.add(line.strip())
 
-    return _compare_sets(name, current_results, expected_results)
-
+    _compare_sets(name, current_results, expected_results)
+    
 
 def compare_results_q3(q3_path):
     name = "results_q3"
@@ -142,11 +115,10 @@ def compare_all_results(results: dict[int, list[str]]):
     for client_id in results.keys():
         q1_path, q2_path, q3_path, q4_path = results[client_id]
         resultq1 = compare_results_q1(q1_path)
-        resultq2_te = compare_results_q2_top_earners(q2_path)
-        resultq2_bs = compare_results_q2_best_sellers(q2_path)
+        resultsq2 = compare_results_q2(client_id)
         resultsq3 = compare_results_q3(q3_path)
         resultsq4 = compare_results_q4(q4_path)
-        if not (resultq1 and resultq2_te and resultq2_bs and resultsq3 and resultsq4):
+        if not (resultq1 and resultsq2 and resultsq3 and resultsq4):
             print(f"❌ Resultados del cliente {client_id} no coinciden.")
             result = False
     return result
