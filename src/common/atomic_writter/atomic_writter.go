d24ecc7d82d8d0376b5a4df85000e5c84b43fc1a
Jignacio14 <j14castro2001@gmail.com>
2025-11-16 14:29:11 -0300

    feat: add atomic writter

---
+++ src/common/atomic_writter/atomic_writter.go
@@ -0,0 +1,52 @@
+package atomicwritter
+
+import (
+	"os"
+	"path/filepath"
+	"strconv"
+	"time"
+)
+
+type AtomicWriter struct {
+	path string
+}
+
+func NewAtomicWriter(path string) *AtomicWriter {
+	return &AtomicWriter{path: path}
+}
+
+func (aw *AtomicWriter) Write(data []string) error {
+	dir := filepath.Dir(aw.path)
+
+	tmpDirName := "tmp_" + filepath.Base(dir) + strconv.FormatInt(time.Now().UnixNano(), 10)
+	tmpFile, err := os.CreateTemp(dir, tmpDirName)
+
+	if err != nil {
+		return err
+	}
+
+	defer os.Remove(tmpFile.Name())
+
+	for i := range data {
+		if _, err = tmpFile.WriteString(data[i]); err != nil {
+			tmpFile.Close()
+			return err
+		}
+	}
+
+	if err = tmpFile.Sync(); err != nil {
+		tmpFile.Close()
+		return err
+	}
+	if err = tmpFile.Close(); err != nil {
+		return err
+	}
+
+	tmpDirName = tmpFile.Name()
+
+	return os.Rename(tmpDirName, aw.path)
+}
+
+func (aw *AtomicWriter) Recover() ([]string, error) {
+	return nil, nil
+}
