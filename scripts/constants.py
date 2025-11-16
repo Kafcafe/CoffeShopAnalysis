42d2ac83078a1d9b3acd4630ba7e92a0a5f9c6c6
Ignacio <j14castro2001@gmail.com>
2025-11-16 19:44:35 -0300

    feat: add separated volumes to each node

--- scripts/constants.py
+++ scripts/constants.py
@@ -164,6 +164,7 @@ GROUP_TEMPLATE = """
         volumes:
           - ./src/group/config.yaml:/config.yaml 
           - /var/run/docker.sock:/var/run/docker.sock
+          - ./processed_data:/processed_data/{group_type}_{id}
 """
 
 JOIN_TEMPLATE = """
