8b9934e910e54fbef6a9730d3201e04822564b47
gabrieldiem <gabriel.diem5@gmail.com>
2025-11-16 12:34:12 -0300

    feat: boom script sends SIGKILL

--- scripts/boom/commands.py
+++ scripts/boom/commands.py
@@ -1,2 +1,2 @@
 DOCKER_SERVICES = """docker compose -f {compose} config --services"""
-DOCKER_KILL = """docker compose -f {compose} stop {dead_man} --timeout 0"""
+DOCKER_KILL = """docker compose -f {compose} kill -s SIGKILL {dead_man}"""
