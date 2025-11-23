056ad137c4d7aa62f2f12cbdafc8873174c69e4f
ramiro gestoso <rgestoso@fi.uba.ar>
2025-11-23 16:13:53 -0300

    use fmt for hashvalue

--- src/common/middleware/utils.go
+++ src/common/middleware/utils.go
@@ -3,11 +3,11 @@ package middleware
 import (
 	"crypto/sha256"
 	"encoding/binary"
-	"encoding/hex"
+	"fmt"
 )
 
 func GetMessageId(data []byte, count int) (string, int) {
 	hashValue := sha256.Sum256(data)
 	partitionKey := binary.BigEndian.Uint64(hashValue[:8])
-	return hex.EncodeToString(hashValue[:]), int(partitionKey%uint64(count)) + 1
+	return fmt.Sprintf("%x", hashValue), int(partitionKey%uint64(count)) + 1
 }
