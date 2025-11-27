41cf7bd4543341b7e474ff4d1257f6f7b16e878f
Ramiro Gestoso <rgestoso@fi.uba.ar>
2025-11-27 00:53:36 -0300

    do not write state in topk

--- src/group/lib/persistence.go
+++ src/group/lib/persistence.go
@@ -11,17 +11,15 @@ func (g *GroupByGenericWorker) dumpData(msg *middleware.Message, hash string, da
 		if err := g.atomicWritter.WriteLines(msg.Payload, metadata); err != nil {
 			return fmt.Errorf("error writing grouped data to file for client %s: %v", msg.ClientId, err)
 		}
-		return nil
-	}
-
-	toSave := g.group.ToFullStringList(msg.ClientId)
-	state := middleware.NewWorkerState(g.clientsStats[msg.ClientId], toSave)
-	jsonStr := state.ToJson()
-	if jsonStr == "" {
-		return fmt.Errorf("error serializing state to JSON for client %s", msg.ClientId)
-	}
-	if err := g.atomicWritter.WriteLine(jsonStr, ".json", []string{msg.ClientId, "WorkerState"}); err != nil {
-		return fmt.Errorf("error writing cache to file for client %s: %v", msg.ClientId, err)
+	} else {
+		data := g.group.ToFullStringList(msg.ClientId)
+		stateJson := middleware.NewWorkerState(g.clientsStats[msg.ClientId], data).ToJson()
+		if stateJson == "" {
+			return fmt.Errorf("error serializing state to JSON for client %s", msg.ClientId)
+		}
+		if err := g.atomicWritter.WriteLine(stateJson, ".json", []string{msg.ClientId, "WorkerState"}); err != nil {
+			return fmt.Errorf("error writing cache to file for client %s: %v", msg.ClientId, err)
+		}
 	}
 	return nil
 }
@@ -36,20 +34,20 @@ func (g *GroupByGenericWorker) recover() {
 	}
 
 	for clientId, data := range data {
-		g.log.Infof("Recovering data for client %s", clientId)
 		if g.conf.ofType == GROUP_TYPE_TOPK {
+			g.log.Warningf("Recovering data (%d) for client %s of datatype: %s", data.GetCount(), clientId, data.GetDataType())
 			g.group.Add(clientId, data.GetData())
 			g.getClientStats(clientId).SetCount(data.GetDataType(), data.GetCount())
-		} else {
+		} else { // is worker state
+			g.log.Warningf("Recovering state for client %s", clientId)
 			jsonStr := data.GetData()[0]
 			state, err := middleware.NewWorkerStateFromJson(jsonStr)
 			if err != nil {
-				g.log.Errorf("Error deserializing state for client %s: %v", clientId, err)
+				g.log.Errorf("Error deserializing state for client %s: %v\n%s", clientId, err, jsonStr)
 				continue
 			}
 			g.clientsStats[clientId] = state.ClientStats
 			g.group.AddFullStringList(clientId, state.Data)
 		}
 	}
-
 }
