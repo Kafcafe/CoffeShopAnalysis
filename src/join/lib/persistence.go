528269a89848524e7090895ab548d7668ebc09d5
Jignacio14 <j14castro2001@gmail.com>
2025-11-30 21:18:11 -0300

    feat: add recover logic

--- src/join/lib/persistence.go
+++ src/join/lib/persistence.go
@@ -37,11 +37,27 @@ func (j *JoinGenericWorker) Recover() error {
 
 		j.mutex.Lock()
 		j.clientsStats[clientId] = clientStats
+
+		// Restore the worker's maps from the recovered data
+		j.sideTable[clientId] = make([]string, len(clientStats.sideTable))
+		copy(j.sideTable[clientId], clientStats.sideTable)
+
+		j.mainTable[clientId] = make([]string, len(clientStats.mainTable))
+		copy(j.mainTable[clientId], clientStats.mainTable)
+
+		// If side table is ready, initialize the channel
+		if clientStats.sideTableIsReady {
+			if _, exists := j.sideTableReceived[clientId]; !exists {
+				j.sideTableReceived[clientId] = make(chan int, SINGLE_ITEM_BUFFER_LEN)
+			}
+		}
+
 		j.log.Infof("Resurecting client: %v", clientId)
 		j.log.Info(clientStats.sideTable)
 		j.log.Info(clientStats.mainTable)
-		j.log.Infof("Side table is ready: %d", clientStats.sideTableIsReady)
+		j.log.Infof("Side table is ready: %t", clientStats.sideTableIsReady)
 		j.log.Infof("Resurrected tables for client %s: mainTable=%d, sideTable=%d", clientId, len(clientStats.mainTable), len(clientStats.sideTable))
+		j.log.Infof("Worker maps synchronized - mainTable=%d, sideTable=%d", len(j.mainTable[clientId]), len(j.sideTable[clientId]))
 		j.mutex.Unlock()
 	}
 
