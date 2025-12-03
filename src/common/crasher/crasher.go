ac15c5cfd62ec29bcac0334173cc13b41a3567e3
Ramiro Gestoso <rgestoso@fi.uba.ar>
2025-12-03 01:49:08 -0300

    remove unnecessary fields from crasher

--- src/common/crasher/crasher.go
+++ src/common/crasher/crasher.go
@@ -2,7 +2,6 @@ package crasher
 
 import (
 	"common/logger"
-	"hash/fnv"
 	"math/rand"
 	"os"
 	"time"
@@ -13,36 +12,25 @@ import (
 const FORCE_EXIT_PROBABILITY float64 = 0.00005 // 1 in 20000
 
 type Crasher struct {
-	rng      *rand.Rand
-	nodeType string
-	id       string
-	logger   *logging.Logger
-	enabled  bool
+	rng     *rand.Rand
+	logger  *logging.Logger
+	enabled bool
 }
 
-func NewCrasher(nodeType, id string, enabled bool) *Crasher {
+func NewCrasher(enabled bool) *Crasher {
 	seed := time.Now().UnixNano() ^
-		int64(rand.Int63()) ^
-		int64(hashString(nodeType+id))
+		int64(rand.Int63())
 
 	return &Crasher{
-		rng:      rand.New(rand.NewSource(seed)),
-		nodeType: nodeType,
-		id:       id,
-		logger:   logger.GetLoggerWithPrefix("[CRASHER]"),
-		enabled:  enabled,
+		rng:     rand.New(rand.NewSource(seed)),
+		logger:  logger.GetLoggerWithPrefix("[CRASHER]"),
+		enabled: enabled,
 	}
 }
 
 func (c *Crasher) ThrowDiceAndForceExit(message string) {
 	if c.enabled && c.rng.Float64() < FORCE_EXIT_PROBABILITY {
-		c.logger.Errorf("[%s%s] Force exit: %s", c.nodeType, c.id, message)
+		c.logger.Errorf("Force exit: %s", message)
 		os.Exit(1)
 	}
-}
-
-func hashString(s string) uint64 {
-	h := fnv.New64a()
-	h.Write([]byte(s))
-	return h.Sum64()
 }
